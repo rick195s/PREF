b562bf0339adf9cd4ed51169c15efb09f26c70d6
Ricardo Franco <ricardosf195@gmail.com>
2023-03-05 20:41:18 +0000

    ConfigBean created

--- backend/src/main/java/pt/ipleiria/estg/dei/ei/pref/HelloResource.java
+++ backend/src/main/java/pt/ipleiria/estg/dei/ei/pref/HelloResource.java
@@ -1,8 +1,9 @@
 package pt.ipleiria.estg.dei.ei.pref;
 
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 
 @Path("/hello-world")
 public class HelloResource {
