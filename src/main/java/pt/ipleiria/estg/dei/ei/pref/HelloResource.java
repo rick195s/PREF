459cdf73fe0cf14dbe33c35da3b019156cec210d
Ricardo Franco <ricardosf195@gmail.com>
2023-03-16 21:53:40 +0000

    Update HelloResource.java

--- src/main/java/pt/ipleiria/estg/dei/ei/pref/HelloResource.java
+++ src/main/java/pt/ipleiria/estg/dei/ei/pref/HelloResource.java
@@ -10,6 +10,6 @@ public class HelloResource {
     @GET
     @Produces("text/plain")
     public String hello() {
-        return "Hello, World! version 23";
+        return "Hello, World! version 24";
     }
 }
