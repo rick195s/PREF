acec7a3b904e3f02449ddbe5387e3f47e705042e
Ricardo Franco <ricardosf195@gmail.com>
2023-03-21 17:43:02 +0000

    orders table in dashboard

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,21 +2,21 @@ module.exports = {
   root: true,
   env: {
     node: true,
-    browser: true,
+    browser: true
   },
   extends: [
     "plugin:vue/recommended",
     "eslint:recommended",
     "prettier",
-    "plugin:nuxt/recommended",
+    "plugin:nuxt/recommended"
   ],
   rules: {
     "vue/component-name-in-template-casing": ["error", "PascalCase"],
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-    "@typescript-eslint/no-unused-vars": ["off"],
+    "@typescript-eslint/no-unused-vars": ["off"]
   },
   globals: {
-    $nuxt: true,
-  },
+    $nuxt: true
+  }
 };
