a406b7a1807b0228c8d73809bf8f9293d5b23032
Sara Martins <2201757@my.ipleiria.pt>
2023-04-10 17:17:04 +0100

    PREF-31-Map appears

--- pages/packages/[id].vue
+++ pages/packages/[id].vue
@@ -3,10 +3,12 @@
     <div class="flex flex-wrap mt-4">
       <div class="w-full mb-12 xl:mb-0 px-4">
         <CardHistory />
+        <MapExample/>
       </div>
     </div>
   </div>
 </template>
 <script setup>
 import CardHistory from "~/components/Cards/CardPackageLogs.vue";
+import MapExample from "~/components/Maps/MapContainer.vue";
 </script>
