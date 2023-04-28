a84858dddb6fe2f82c991da092c60a03eeba614a
Ricardo Franco <ricardosf195@gmail.com>
2023-04-28 17:14:36 +0100

    PREF-48 configuração desnecessária de runtime removida

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,10 +20,4 @@ export default defineNuxtConfig({
     },
  
     css: ['~/assets/styles/index.css', '@fortawesome/fontawesome-free/css/all.min.css', '@/assets/styles/tailwind.css'],
-    runtimeConfig: {
-        // Keys within public, will be also exposed to the client-side
-        public: {
-            apiUrl: process.env.API_BASE_URL || 'https://pref.azurewebsites.net/pref/api',
-        },
-    },
 });
