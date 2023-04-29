04346a021a2cf280e971b8cab28a0fcd3b5fa38b
Ricardo Franco <ricardosf195@gmail.com>
2023-04-29 13:24:22 +0100

    vite proxy alterado para nitro proxy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,18 +6,13 @@ export default defineNuxtConfig({
             autoprefixer: {},
         },
     },
-    vite: {
-        server: {
-            
-            proxy: {
-                '/api': {
-                    target: process.env.API_BASE_URL || 'https://pref.azurewebsites.net/pref/api',
-                    changeOrigin: true,
-                    rewrite: (path) => path.replace(/^\/api/, ''),
-                },
+    nitro:{
+        devProxy: {
+            '/api': {
+                target: process.env.API_BASE_URL || 'https://pref.azurewebsites.net/pref/api',
+                changeOrigin: true,
             },
         },
     },
- 
     css: ['~/assets/styles/index.css', '@fortawesome/fontawesome-free/css/all.min.css', '@/assets/styles/tailwind.css'],
 });
