d6444836d9ead86c6e035a3fe8f6340bcba46191
Ricardo Franco <ricardosf195@gmail.com>
2023-05-02 11:44:52 +0100

    PREF-44 remover embalagens do draggable

--- components/Cards/CardSelectedProductPackages.vue
+++ components/Cards/CardSelectedProductPackages.vue
@@ -9,14 +9,22 @@
         <div
           class="bg-white flex flex-row justify-between px-6 align-middle text-xs whitespace-nowrap p-4"
         >
+          <i class="fas fa-bars"></i>
           <div>
             {{ element.name }}
           </div>
           <div>
             {{ element.type }}
           </div>
+
           <div>
-            <i class="fas fa-bars"></i>
+            <button
+              class="bg-red-500 text-white active:bg-red-600 font-bold uppercase px-1 rounded shadow hover:shadow-md outline-none focus:outline-none"
+              type="button"
+              @click="removePackage(element)"
+            >
+              <i class="fas fa-minus"></i>
+            </button>
           </div>
         </div>
       </template>
@@ -48,4 +56,11 @@ const selectedPackages = computed({
     emit("update:modelValue", value);
   }
 });
+
+const removePackage = (element) => {
+  const index = selectedPackages.value.indexOf(element);
+  if (index > -1) {
+    selectedPackages.value.splice(index, 1);
+  }
+};
 </script>
