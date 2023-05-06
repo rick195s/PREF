aa5f619b27a644965c88c879898f3438d3c33ce4
Sara Martins <2201757@my.ipleiria.pt>
2023-05-06 13:38:39 +0100

    CardOrderLinesPackages Created

--- pages/orders/[trackingNumber].vue
+++ pages/orders/[trackingNumber].vue
@@ -2,11 +2,13 @@
   <div class="flex flex-wrap mt-4">
     <div class="w-full mb-12 xl:mb-0 px-4">
       <CardOrderDetails />
-      <CardsCardOrderLines />
+      <CardOrderLines />
+      <CardOrderLinePackages />
     </div>
   </div>
 </template>
 <script setup>
 import CardOrderDetails from "@/components/Cards/CardOrderDetails.vue";
-import CardsCardOrderLines from "@/components/Cards/CardOrderLines.vue";
+import CardOrderLines from "@/components/Cards/CardOrderLines.vue";
+import CardOrderLinePackages from "~/components/Cards/CardOrderLinesPackages.vue";
 </script>
