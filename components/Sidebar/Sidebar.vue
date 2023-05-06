0051a9425773c29208ae5c0612a0669358e02c7c
Ricardo Franco <ricardosf195@gmail.com>
2023-05-07 00:54:37 +0100

    logo prozis adicionado

--- components/Sidebar/Sidebar.vue
+++ components/Sidebar/Sidebar.vue
@@ -18,7 +18,11 @@
         class="md:block text-left md:pb-2 text-blueGray-600 mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold p-4 px-0"
         to="/"
       >
-        Embalagens do Futuro
+        <img
+          alt="logo"
+          class="mx-auto w-64 align-middle"
+          src="~/assets/img/prozis_logo_red.png"
+        />
       </nuxt-link>
 
       <ul class="md:hidden items-center flex flex-wrap list-none">
