174327160a81715ace99a987f65938d7e4e755cf
Ricardo Franco <ricardosf195@gmail.com>
2023-05-11 12:34:24 +0100

    PREF-60 verificar se ja existe quantity ou category antes de criar

--- src/main/java/pt/ipleiria/estg/dei/ei/pref/ejbs/pattern/ObservationBean.java
+++ src/main/java/pt/ipleiria/estg/dei/ei/pref/ejbs/pattern/ObservationBean.java
@@ -10,6 +10,7 @@ import pt.ipleiria.estg.dei.ei.pref.enumerators.PhenomenonType;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import java.util.List;
 
@@ -47,16 +48,34 @@ public class ObservationBean {
         }
 
         if (phenomenonType.isMeasurement()) {
-            double measurementValue = Double.parseDouble(value);
-            Quantity quantity = new Quantity(measurementValue);
-            entityManager.persist(quantity);
-            MeasurementObservation measurementObservation = new MeasurementObservation(phenomenonType, observer, date, details, observablePackage, quantity);
-            entityManager.persist(measurementObservation);
-            return measurementObservation;
+            return createMeasurementObservation(phenomenonType, observer, date, details, observablePackage, value);
         }
 
-        Category category = new Category(value);
-        entityManager.persist(category);
+        return createCategoryObservation(phenomenonType, observer, date, details, observablePackage, value);
+    }
+
+    private MeasurementObservation createMeasurementObservation(PhenomenonType phenomenonType, Observer observer, String date, String details, ObservablePackage observablePackage, String value) {
+        double measurementValue = Double.parseDouble(value);
+        Quantity quantity = null;
+        try {
+            quantity = (Quantity) entityManager.createNamedQuery("getQuantityByValue").setParameter("value", measurementValue).getSingleResult();
+        } catch (NoResultException e) {
+            quantity = new Quantity(measurementValue);
+            entityManager.persist(quantity);
+        }
+        MeasurementObservation measurementObservation = new MeasurementObservation(phenomenonType, observer, date, details, observablePackage, quantity);
+        entityManager.persist(measurementObservation);
+        return measurementObservation;
+    }
+
+    private CategoryObservation createCategoryObservation(PhenomenonType phenomenonType, Observer observer, String date, String details, ObservablePackage observablePackage, String value) {
+        Category category = null;
+        try {
+            category = (Category) entityManager.createNamedQuery("getCategoryByValue").setParameter("value", value).getSingleResult();
+        } catch (NoResultException e) {
+            category = new Category(value);
+            entityManager.persist(category);
+        }
         CategoryObservation categoryObservation = new CategoryObservation(phenomenonType, observer, date, details, observablePackage, category);
         entityManager.persist(categoryObservation);
         return categoryObservation;
