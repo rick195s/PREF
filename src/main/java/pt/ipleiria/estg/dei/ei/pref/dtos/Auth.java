31bb0de2868932760672d80291752c138669154c
Ricardo Franco <ricardosf195@gmail.com>
2023-06-08 10:56:49 +0100

    PREF-85 user classe adicionada

---
+++ src/main/java/pt/ipleiria/estg/dei/ei/pref/dtos/Auth.java
@@ -0,0 +1,34 @@
+package pt.ipleiria.estg.dei.ei.pref.dtos;
+
+import javax.validation.constraints.NotBlank;
+
+public class Auth {
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    public Auth() {}
+
+    public Auth(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/main/java/pt/ipleiria/estg/dei/ei/pref/enumerators/Role.java
@@ -0,0 +1,8 @@
+package pt.ipleiria.estg.dei.ei.pref.enumerators;
+
+public enum Role {
+    ADMINISTRATOR,
+    CLIENT,
+    REPAIR_SHOP_EXPERT,
+    INSURER_EXPERT
+}
